0946b526a12a560b5b1845f67ef7fd20349cde71
YX <yxxinyuan@zju.edu.cn>
2021-04-28 00:26:02 +0800

    Add Diff

---
+++ CSL/CSL-r/public/csldiff.h
@@ -0,0 +1,335 @@
+﻿/*
+** Gao Chengxiang, 2021, BSD(2)
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef __CSL_DIFF_H__
+#define __CSL_DIFF_H__
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __CSL_BASE_H__
+	#error csldiff.h requires cslbase.h to be included first.
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+namespace CSL {
+////////////////////////////////////////////////////////////////////////////////
+
+// myers algorithm
+
+typedef std::pair<int, int> myers_point;
+
+//find middle point from start to end in edit graph (a, b)
+template <class TIterator>
+inline void myers_find_middle(const typename TIterator::value_type* a, const typename TIterator::value_type* b,
+							const myers_point& start, const myers_point& end,
+							std::vector<int>& vecForward, std::vector<int>& vecBackward,
+							myers_point& pt) noexcept
+{
+	/*
+	start 1 2 ... n
+	1
+	2
+	...
+	m             end
+	*/
+	int n = end.first - start.first;    //x
+	int m = end.second - start.second;  //y
+
+	//initialize
+	for(int i = 0; i <= n + m; ++ i)
+		vecForward[i] = -1;
+	vecForward[m + 1] = 0;
+	for(int i = 0; i <= n + m; ++ i)
+		vecBackward[i] = -1;
+	vecBackward[n - 1] = n;
+
+	for(int d = 0; d <= (n + m + 1) / 2; d++) {
+		//forward
+		for(int k = -m + ((d^m)&1), kInv = k + m; k <= n; k += 2, kInv = k + m) {
+			int x, y;
+			if((kInv == 0 || vecForward[kInv - 1] < 0) && (kInv == m+n || vecForward[kInv + 1] < 0)) {
+				continue;
+			}
+			else if(kInv == 0 || vecForward[kInv - 1] < 0 || (kInv != n + m && vecForward[kInv + 1] >= 0 && vecForward[kInv - 1] < vecForward[kInv + 1])) {
+				//now: move from top (k+1)
+				x = vecForward[kInv + 1];
+				y = x - k;
+			}
+			else {
+				//now: move from left (k-1)
+				x = vecForward[kInv - 1] + 1;
+				y = x - k;
+			}
+			if(x > n || y > m || x < 0 || y < 0)
+				continue;
+			int endx = x, endy = y;
+			while(endx < n && endy < m && a[endx + start.first] == b[endy + start.second])
+				endx++, endy++;
+			vecForward[kInv] = endx;
+			if(vecForward[kInv] >= 0 && vecBackward[kInv] >= 0 && vecForward[kInv] >= vecBackward[kInv]) {
+				pt.first = x + start.first;
+				pt.second = y + start.second;
+				return ;
+			}
+		}
+
+		//backward
+		for(int k = -m + ((d^n)&1), kInv = k + m; k <= n; k += 2, kInv = k + m) {
+			int x, y;
+			if((kInv == 0 || vecBackward[kInv - 1] < 0) && (kInv == m+n || vecBackward[kInv + 1] < 0)) {
+				continue;
+			}
+			else if(kInv == m + n || vecBackward[kInv + 1] < 0 || (kInv != 0 && vecBackward[kInv - 1] >= 0 && vecBackward[kInv - 1] < vecBackward[kInv + 1])) {
+				//move from bottom
+				x = vecBackward[kInv - 1];
+				y = x - k;
+			}
+			else {
+				//move from right
+				x = vecBackward[kInv + 1] - 1;
+				y = x - k;
+			}
+			if(x > n || y > m || x < 0 || y < 0)
+				continue;
+			while(x > 0 && y > 0 && a[x - 1 + start.first] == b[y - 1 + start.second])
+				x--, y--;
+			vecBackward[kInv] = x;
+			if(vecForward[kInv] >= 0 && vecBackward[kInv] >= 0 && vecForward[kInv] >= vecBackward[kInv]) {
+				pt.first = x + start.first;
+				pt.second = y + start.second;
+				return ;
+			}
+		}
+	}
+	pt = end;
+}
+
+//Longest common sequence
+struct LcsItem
+{
+	int iStart1;  //based 0
+	int iEnd1;
+	int iStart2;  //based 0
+	int iEnd2;
+};
+
+//myers algorithm
+
+template <typename TIterator>
+inline void MyersDiff(const TIterator& a_begin, const TIterator& a_end,
+					const TIterator& b_begin, const TIterator& b_end,
+					std::vector<LcsItem>& vecLCS)
+{
+	//length
+    auto a_size = std::distance(a_begin, a_end);
+    auto b_size = std::distance(b_begin, b_end);
+	assert( a_size >= 0 && b_size >= 0 );
+	if( a_size > (std::numeric_limits<int>::max)()
+		|| b_size > (std::numeric_limits<int>::max)() )
+		throw std::overflow_error("");
+	int n = (int)a_size;
+	int m = (int)b_size;
+
+	//V[k]: forward & backward
+	std::vector<int> vecForward;
+	std::vector<int> vecBackward;
+	{
+		//n+m+2
+		int count = SafeOperators::AddThrow(n, m);
+		count = SafeOperators::AddThrow(count, (int)2);
+		vecForward.resize(count);
+		vecBackward.resize(count);
+	}
+
+	//result
+	vecLCS.clear();
+
+	//add all or delete all
+	if( n == 0 || m == 0 )
+		return ;
+
+	//division stack
+	std::stack<myers_point> vecSearchStack;
+	//edit path
+	std::vector<myers_point> vecEditPath;
+
+	//initialize
+	myers_point start = std::make_pair(0, 0);
+	myers_point end = std::make_pair(n, m);
+	myers_point middle;
+
+	//loop
+	do {
+		//same sequence at beginning
+		while(start.first != end.first && start.second != end.second && *(&(*a_begin)+start.first) == *(&(*b_begin)+start.second)) {
+			start.first++;
+			start.second++;
+			vecEditPath.push_back(start); //based 1
+		}
+		//current n < current m
+		while(start.first == end.first && start.second != end.second) {
+			start.second++;
+			vecEditPath.push_back(start);
+		}
+		//current n > current m
+		while(start.first != end.first && start.second == end.second) {
+			start.first++;
+			vecEditPath.push_back(start);
+		}
+
+		//search
+		if(start != end) {
+			myers_find_middle<TIterator>(&(*a_begin), &(*b_begin), start, end, vecForward, vecBackward, middle);
+			vecSearchStack.push(end);
+			end = middle;
+		}
+		else {
+			if(!vecSearchStack.empty()) {
+				end = vecSearchStack.top();
+				vecSearchStack.pop();
+			}
+		}
+	} while(start != end || !vecSearchStack.empty());
+
+	//to LCS
+	start = std::make_pair(0, 0);
+	for( auto& it : vecEditPath ) {
+		//the same
+        if(it.first == start.first + 1 && it.second == start.second + 1) {
+			if(vecLCS.size() != 0) {
+				LcsItem& item(vecLCS[vecLCS.size() - 1]);
+				if(item.iEnd1 + 1 == start.first && item.iEnd2 + 1 == start.second) {
+					item.iEnd1 = start.first;
+					item.iEnd2 = start.second;
+					start = it;
+					continue;
+				}
+			}
+			LcsItem item;
+			item.iStart1 = item.iEnd1 = start.first;
+			item.iStart2 = item.iEnd2 = start.second;
+			vecLCS.push_back(item);
+		}
+		else {
+			assert( (it.first == start.first && it.second == start.second + 1) // add start.second
+					|| (it.first == start.first + 1 && it.second == start.second) // delete start.first
+				);
+		}
+		start = it;
+	}
+}
+
+//Edit types
+enum {
+	Diff_Add = 0,
+	Diff_Delete,
+	Diff_Revise
+};
+
+//Diff item
+struct DiffItem
+{
+	int iType;
+	int iStart1;  //based 0
+	int iEnd1;
+	int iStart2;  //based 0
+	int iEnd2;
+};
+
+inline void LcsToDiff(const std::vector<LcsItem>& vecLCS, int n, int m, std::vector<DiffItem>& vecDiff)
+{
+	vecDiff.clear();
+	if( n == 0 && m == 0 )
+		return ;
+
+	DiffItem ditem;
+	ditem.iStart1 = 0;
+	ditem.iStart2 = 0;
+	if( n == 0 ) {
+		ditem.iType = Diff_Add;
+		ditem.iEnd1 = 0;
+		ditem.iEnd2 = m - 1;
+		vecDiff.push_back(ditem);
+		return ;
+	}
+	if( m == 0 ) {
+		ditem.iType = Diff_Delete;
+		ditem.iEnd1 = n - 1;
+		ditem.iEnd2 = 0;
+		vecDiff.push_back(ditem);
+		return ;
+	}
+	if( vecLCS.size() == 0 ) {
+		ditem.iType = Diff_Revise;
+		ditem.iEnd1 = n - 1;
+		ditem.iEnd2 = m - 1;
+		vecDiff.push_back(ditem);
+		return ;
+	}
+
+	//loop
+	int iLast1 = -1;
+	int iLast2 = -1;
+	for( size_t i = 0; i < vecLCS.size(); i ++ ) {
+		const LcsItem& sitem(vecLCS[i]);
+		ditem.iStart1 = iLast1 + 1;
+		ditem.iStart2 = iLast2 + 1;
+		if( sitem.iStart1 == iLast1 + 1 ) {
+			if( (i == 0 && sitem.iStart2 != 0) || i != 0 ) {
+				assert( (i == 0) || (sitem.iStart2 != iLast2 + 1) );
+				ditem.iType = Diff_Add;
+				ditem.iEnd1 = iLast1 + 1;
+				ditem.iEnd2 = sitem.iStart2 - 1;
+				vecDiff.push_back(ditem);
+			}
+		}
+		else {
+			if( sitem.iStart2 == iLast2 + 1 ) {
+				ditem.iType = Diff_Delete;
+				ditem.iEnd1 = sitem.iStart1 - 1;
+				ditem.iEnd2 = iLast2 + 1;
+				vecDiff.push_back(ditem);
+			}
+			else {
+				ditem.iType = Diff_Revise;
+				ditem.iEnd1 = sitem.iStart1 - 1;
+				ditem.iEnd2 = sitem.iStart2 - 1;
+				vecDiff.push_back(ditem);
+			}
+		}
+		iLast1 = sitem.iEnd1;
+		iLast2 = sitem.iEnd2;
+	}
+	//tail
+	ditem.iStart1 = iLast1 + 1;
+	ditem.iStart2 = iLast2 + 1;
+	if( iLast1 == n - 1 ) {
+		if( iLast2 < m - 1 ) {
+			ditem.iType = Diff_Add;
+			ditem.iEnd1 = iLast1 + 1;
+			ditem.iEnd2 = m - 1;
+			vecDiff.push_back(ditem);
+		}
+	}
+	else {
+		if( iLast2 == m - 1 ) {
+			ditem.iType = Diff_Delete;
+			ditem.iEnd1 = n - 1;
+			ditem.iEnd2 = iLast2 + 1;
+			vecDiff.push_back(ditem);
+		}
+		else {
+			ditem.iType = Diff_Revise;
+			ditem.iEnd1 = n - 1;
+			ditem.iEnd2 = m - 1;
+			vecDiff.push_back(ditem);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+}
+////////////////////////////////////////////////////////////////////////////////
+#endif
+////////////////////////////////////////////////////////////////////////////////
---
+++ CSL/CSL-r/public/cslmem.h
@@ -0,0 +1,231 @@
+﻿/*
+** Xin YUAN, 2021, BSD(2)
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef __CSL_MEM_H__
+#define __CSL_MEM_H__
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __CSL_BASE_H__
+	#error cslmem.h requires cslbase.h to be included first.
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+namespace CSL {
+////////////////////////////////////////////////////////////////////////////////
+
+// fixed_element_memory_pool
+
+class fixed_element_memory_pool
+{
+public:
+	fixed_element_memory_pool() noexcept : m_pHead(NULL)
+	{
+	}
+	fixed_element_memory_pool(const fixed_element_memory_pool& src) = delete;
+	fixed_element_memory_pool& operator=(const fixed_element_memory_pool& src) = delete;
+	~fixed_element_memory_pool() noexcept
+	{
+		FreeDataChain();
+	}
+
+//methods
+
+	// free data links
+	void FreeDataChain() noexcept
+	{
+		void* pPlex = m_pHead;
+		while( pPlex != NULL ) {
+			void* pNext = get_next_block(pPlex);
+			::free(pPlex);
+			pPlex = pNext;
+		}
+		m_pHead = NULL;
+	}
+
+	// create block
+	void* CreateBlock(uintptr_t uMinElements, uintptr_t uMaxElements, uintptr_t uElementSize, uintptr_t& uActElements)  //may throw
+	{
+		assert( uMinElements > 0 && uMaxElements > 0 && uMinElements <= uMaxElements && uElementSize > 0 );
+
+		void* pPlex = NULL;
+		uintptr_t uBytes = 0;
+		uintptr_t uElements = uMaxElements;
+		uintptr_t uLinkSize = sizeof(void*);
+
+		uActElements = uElements;
+		while( uElements >= uMinElements ) {
+			//try
+			if( !SafeOperators::Multiply<uintptr_t>(uElements, uElementSize, uBytes)
+				|| !SafeOperators::Add<uintptr_t>(uBytes, uLinkSize, uBytes) ) {
+				uElements --;
+				continue;
+			}
+			break;
+		}
+		if( uElements < uMinElements )
+			throw std::overflow_error("");
+
+		while( uElements >= uMinElements ) {
+			//no overflow
+			uBytes = uElements * uElementSize + sizeof(void*);
+			pPlex = ::malloc(uBytes);
+			if( pPlex == NULL ) {
+				uElements --;
+				continue;
+			}
+			break;
+		}
+		if( pPlex == NULL )
+			throw std::bad_alloc("");
+		uActElements = uElements;
+
+		get_next_block(pPlex) = m_pHead;
+		m_pHead = pPlex;
+
+		return get_data(pPlex);
+	}
+
+private:
+	static void*& get_next_block(void* pBlock) noexcept
+	{
+		assert( pBlock != NULL );
+		return *((void**)pBlock);
+	}
+	static void* get_data(void* pBlock) noexcept
+	{
+		return (void*)(((void**)pBlock) + 1);
+	}
+
+private:
+	void* m_pHead;
+};
+
+// free_list<TNode>
+//  TNode : has a member named m_pNext (TNode*)
+
+template <class TNode>
+class free_list
+{
+public:
+	explicit free_list(uintptr_t uMinElements = 10, uintptr_t uMaxElements = 10) noexcept
+					: m_pFree(NULL), m_uMinBlockElements(uMinElements), m_uMaxBlockElements(uMaxElements)
+	{
+		assert( uMinElements > 0 && uMaxElements > 0 && uMinElements <= uMaxElements );
+	}
+	free_list(const free_list&) = delete;
+	free_list& operator=(const free_list&) = delete;
+	~free_list() noexcept
+	{
+	}
+
+//methods
+
+	void Clear() noexcept
+	{
+		m_pFree = NULL;
+		m_pool.FreeDataChain();
+	}
+	//get a free node
+	TNode* GetFreeNode()  //may throw
+	{
+		if( m_pFree == NULL ) {
+			uintptr uActElements;
+			//may throw
+			TNode* pNode = (TNode*)m_pool.CreateBlock(m_uMinBlockElements, m_uMaxBlockElements, sizeof(TNode), uActElements);
+			pNode += (uActElements - 1);
+			for( uintptr_t uBlock = uActElements; uBlock > 0; uBlock -- ) {
+				pNode->m_pNext = m_pFree;
+				m_pFree = pNode;
+				pNode --;
+			}
+		}
+		assert( m_pFree != NULL );
+		return m_pFree;
+	}
+	//pick free node
+	void PickFreeNode() noexcept
+	{
+		if( m_pFree != NULL )
+			m_pFree = m_pFree->m_pNext;
+	}
+	void PutFreeNode(TNode* pNode) noexcept
+	{
+		pNode->m_pNext = m_pFree;
+		m_pFree = pNode;
+	}
+
+private:
+	//pool
+	fixed_element_memory_pool  m_pool;
+	TNode*  m_pFree;  //free list
+	uintptr_t m_uMinBlockElements, m_uMaxBlockElements;
+};
+
+// fixed_size_memory_pool<t_size>
+
+template <uintptr_t t_size>
+class fixed_size_memory_pool
+{
+private:
+	struct _Node
+	{
+		_Node* m_pNext;
+		char   m_data[t_size];
+	};
+
+public:
+	explicit fixed_size_memory_pool(uintptr_t uMinElements = 10, uintptr_t uMaxElements = 10) noexcept
+									: m_uElements(0), m_freelist(uMinElements, uMaxElements)
+	{
+	}
+	fixed_size_memory_pool(const fixed_size_memory_pool&) = delete;
+	fixed_size_memory_pool& operator=(const fixed_size_memory_pool&) = delete;
+	~fixed_size_memory_pool() noexcept
+	{
+		//check memory leakage
+		assert( m_uElements == 0 );
+	}
+
+//methods
+
+	void* Allocate() //may throw
+	{
+		_Node* pNode = m_freelist.GetFreeNode();  //may throw
+		m_freelist.PickFreeNode();
+
+		m_uElements ++;
+		assert( m_uElements > 0 );
+
+		return (void*)(pNode->m_data);  //data
+	}
+	void Free(void* p) noexcept
+	{
+		//back to node
+		_Node* pNode = (_Node*)((unsigned char*)p - (intptr_t)((unsigned char*)(((_Node*)(0x128))->m_data) - (unsigned char*)(0x128)));
+		m_freelist.PutFreeNode(pNode);
+
+		assert( m_uElements > 0 );
+		m_uElements --;
+		if( m_uElements == 0 )
+			m_freelist.Clear();
+	}
+
+	void Clear() noexcept
+	{
+		m_freelist.Clear();
+		m_uElements = 0;
+	}
+
+private:
+	uintptr_t m_uElements;
+
+	free_list<_Node>  m_freelist;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+}
+////////////////////////////////////////////////////////////////////////////////
+#endif
+////////////////////////////////////////////////////////////////////////////////
